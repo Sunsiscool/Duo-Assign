ad5196f319d2d8315440ce58bfb056bb5e447271
Sunsiscool <luisenriquemelofranjul@gmail.com>
2026-01-28 13:52:06 -0400

    finished first two functionalities

--- FIle proyect duo.py
+++ FIle proyect duo.py
@@ -1,1 +1,43 @@
-print("Hello from Melos computer")
+def suma(a,b):
+    return a+b
+    
+def resta(a,b):
+    return a-b
+
+#multiplicacion
+
+#division
+
+while True:
+    print("Menu calculator")
+
+    print("1, suma")
+    print("2, resta")
+    #multiplicacion
+
+    #division
+    print("3, salir")
+
+    usuario = input("")
+
+    match usuario:
+        case "1":
+            print("suma")
+            n1 = int(input("inserte el primer numero: "))
+            n2 = int(input("inserte el segundo numero: "))
+
+            print(f"la suma de {n1} + {n2} es = {suma{n1,n1}}")
+
+        case "2":
+            print("resta")
+            n1 = int(input("insete el primer numero: "))
+            n2 = int(input("inserte el segundo numero: "))
+
+            print(f"la resta de {n1} - {n2} es = {suma{n1,n1}}")
+
+        case "3":
+            print("Bye")
+            break
+
+        case _:
+            print("No es una opcion valida")
